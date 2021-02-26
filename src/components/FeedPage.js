8e3f43615a4905b63acd1da8910f7ad30720d74a
annagiuliam <annagiulia.musso@gmail.com>
2021-02-26 12:31:20 +0100

    created feed page

---
+++ src/components/FeedPage.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { BarkerContext } from "../context/BarkerContext";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="sidebar">Sidebar</div>
+      <div className="feed"></div>
+    </div>
+  );
+};
+
+export default Home;
