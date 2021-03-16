743f289d9a3599e5573eda3023357fb89e29763d
annagiuliam <annagiulia.musso@gmail.com>
2021-03-16 12:13:12 +0100

    change in modal

--- src/components/modals/ErrorModal.js
+++ src/components/modals/ErrorModal.js
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import { BarkerContext } from "../../context/BarkerContext";
 
 const ErrorModal = () => {
-  const { showError, error, closeError } = useContext(BarkerContext);
-  const modalDisplay = showError ? "modal display-block" : "modal display-none";
+  const { error, closeError } = useContext(BarkerContext);
+
   return (
-    <div className={modalDisplay}>
+    <div className="modal">
       <section className="modal-main">
         <div className="error-message">{error}</div>
         <button onClick={closeError}>OK</button>
