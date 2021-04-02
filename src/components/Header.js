d4487c1c6347e5f2f909520d96b472ace61a2e84
annagiuliam <annagiulia.musso@gmail.com>
2021-04-02 15:34:37 +0200

    added link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { BarkerContext } from "../context/BarkerContext";
+import { Link } from "react-router-dom";
 
 // import firebaseApp from "../firebase/firebase";
 // import firebase from "firebase/app";
@@ -11,7 +12,9 @@ const Header = () => {
       <div>
         <img className="avatar-img" src={userInfo.url} alt="user avatar"></img>
       </div>
-      <div>{userInfo.username}</div>
+      <Link to={`/profile/${userInfo.uid}`} className="link-username">
+        <span className="username">{userInfo.username}</span>
+      </Link>
       <button onClick={logOut}>Log Out</button>
     </header>
   );
