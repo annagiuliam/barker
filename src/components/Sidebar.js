648be03bd4c647b7f4e595834a55776d6e1f2051
annagiuliam <annagiulia.musso@gmail.com>
2021-04-13 10:52:56 +0200

    sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,8 @@ import { BarkerContext } from "../context/BarkerContext";
 
 import { Router, Link, NavLink, useRouteMatch } from "react-router-dom";
 import { GiBalloonDog } from "react-icons/gi";
+import { FaHome } from "react-icons/fa";
+import { BsPeopleFill, BsPersonFill } from "react-icons/bs";
 
 const Sidebar = (props) => {
   const { currentUser } = useContext(BarkerContext);
@@ -13,28 +15,30 @@ const Sidebar = (props) => {
       <div>
         <GiBalloonDog id="dog-icon" />
       </div>
-      <div>
-        <ul className="sidebar-ul">
-          {/* <NavLink to="/home" activeClassName="selected">
-            <li>Home</li>
-          </NavLink> */}
+      <div className="menu-container">
+        <NavLink activeClassName={"active"} exact={true} to="/home">
+          <div className="sidebar-option">
+            <FaHome />
+            <h3>Home</h3>
+          </div>
+        </NavLink>
 
-          <li>
-            <NavLink activeClassName={"active"} exact={true} to="/home">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={`${url}barkers`} activeClassName={"active"}>
-              <GiBalloonDog />
-              <span>Other barkers</span>
-            </NavLink>
-          </li>
+        <NavLink to={`${url}barkers`} activeClassName={"active"}>
+          <div className="sidebar-option">
+            <BsPeopleFill />
+            <h3>Who to follow</h3>
+          </div>
+        </NavLink>
 
-          <Link to={`${url}profile/${currentUser.uid}`}>
-            <li>Your profile</li>
-          </Link>
-        </ul>
+        <NavLink
+          to={`${url}profile/${currentUser.uid}`}
+          activeClassName={"active"}
+        >
+          <div className="sidebar-option">
+            <BsPersonFill />
+            <h3>Your Profile</h3>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
