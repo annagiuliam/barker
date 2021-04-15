fd66b2008dcf1c8604128aa09441e4ae16a727ef
annagiuliam <annagiulia.musso@gmail.com>
2021-04-15 12:24:11 +0200

    code cleanup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./styles/app.css";
 import "./styles/posts.css";
 import "./styles/signIn.css";
 import "./styles/sidebar.css";
-import "./styles/users.css";
+import "./styles/home.css";
 import "./styles/profile.css";
 import "./styles/buttons.css";
 
